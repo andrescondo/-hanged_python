2d0cb56559b92ac4b2077bca35735733d5ef7464
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-25 15:59:59 -0500

    validacion de aciertos y fallos al ingreso de caracteres

--- hangeds.py
+++ hangeds.py
@@ -108,11 +108,27 @@ def run():
     if len(letter_indexes) == 0 :
       tries += 1
 
+      if tries == 7:
+        display_board(hidden_word, tries)
+        print('')
+        print('¡Perdiste ! La palabra correcta era {}'.format(word))
+        break
+
+
     else: 
       for idx in  letter_indexes:
         hidden_word[idx] = current_letter
 
       letter_indexes = []
+
+      try:
+        hidden_word.index('-')
+      except ValueError:
+        print('')
+        print('¡Felicidades! Ganaste. la palabra es: {}'.format(word))
+        break
+
+
 
 if __name__ == '__main__':
     print('B I E N V E N I D O   A  A H O R C A D O S ')
