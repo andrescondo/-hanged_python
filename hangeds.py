0d3ad901522d69007d3b409ae0ef2b159b6dca88
Andres Condo <1dkevinandres.b@gmail.com>
2020-04-25 13:47:09 -0500

    muestra en pantalla del ASCII art y el ingreso de palabras

--- hangeds.py
+++ hangeds.py
@@ -1,70 +1,107 @@
 # -*- coding: utf-8 -*-
 
+import random
+
 IMAGES = ['''
 
-    +---+
-    |   |
-        |
-        |
-        |
-        |
-        =========''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+      =========''', '''
 
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-        =========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+      =========''', '''
 
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-        =========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+      =========''', '''
 
-    +---+
-    |   |
-    O   |
-   /|   |
-        |
-        |
-        =========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+      =========''', '''
 
-    +---+
-    |   |
-    O   |
-   /|\  |
-        |
-        |
-        =========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+      =========''', '''
 
-    +---+
-    |   |
-    O   |
-   /|\  |
-    |   |
-        |
-        =========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+  |   |
+      |
+      =========''', '''
 
-    +---+
-    |   |
-    O   |
-   /|\  |
-    |   |
-   /    |
-        =========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+  |   |
+ /    |
+      =========''', '''
 
-    +---+
-    |   |
-    O   |
-   /|\  |
-    |   |
-   / \  |
-        =========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+  |   |
+ / \  |
+      =========''', '''
 ''']
 
-if __name__ == '__main__'
+WORDS = [
+  'batman',
+  'python',
+  'javascript',
+  'css',
+  'java',
+  '8cho',
+  'programacion'
+]
+
+def random_word():
+  idx = random.randint(0, len(WORDS) - 1)
+  return WORDS[idx]
+
+
+def display_board(hidden_word, tries):
+  print(IMAGES[tries])
+  print('\n')
+  print(hidden_word)
+  print('\n')
+  print(' -- * -- * -- * -- * -- * -- * ')
+
+
+def run():
+  word = random_word()
+  hidden_word = ['-'] * len(word)
+  tries = 0 
+
+  while True:
+    display_board(hidden_word, tries)
+    current_letter = str(raw_input('Ingrese una palabra: '))
+
+
+if __name__ == '__main__':
+    print('B I E N V E N I D O   A  A H O R C A D O S ')
+    run()
